16b6a5135d5a4fb1180fe8ba6feda75916cc27b3
Farras Nafisya <syaailendrra@gmail.com>
2026-06-09 09:59:23 +0700

    fix root index untuk vercel

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const path = require('path'); // Tambahkan ini
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Supaya server bisa membaca data dari form input HTML
 app.use(express.urlencoded({ extended: true }));
 
-// PENTING UNTUK VERCEL: Arahkan folder views ke lokasi yang benar
-app.set('views', path.join(__dirname, '../views'));
+// Setting EJS normal (karena index.js sudah di luar bersama folder views)
 app.set('view engine', 'ejs');
 
-// Data Menu Makanan (Hardcoded)
+// Data Menu Makanan
 const menu = [
   { id: 1, name: 'Ayam Goreng Awan', price: 25000 },
   { id: 2, name: 'Mie Goreng Nimbus', price: 18000 },
@@ -43,12 +41,12 @@ app.get('/success', (req, res) => {
   res.render('success');
 });
 
-// Jalankan server jika dijalankan secara lokal
+// Jalankan server jika dijalankan secara lokal (di laptop sendiri)
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
     console.log(`Aplikasi foodAwan berjalan di http://localhost:${PORT}`);
   });
 }
 
-// Export app untuk Vercel Serverless
+// WAJIB UNTUK VERCEL: Mengekspor aplikasi Express
 module.exports = app;
